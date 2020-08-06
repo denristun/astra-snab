c56d6772dcc3aefe7bcf29a3c2aa8d7af36624bb
sanddwich <64834314+sanddwich@users.noreply.github.com>
2020-08-06 17:53:22 +0400

    06-08-2020_1kis

--- src/components/RequestPageKis/RPKHeader/index.tsx
+++ src/components/RequestPageKis/RPKHeader/index.tsx
@@ -10,9 +10,8 @@ function headerShift(elem: any, param: any): void {
 class RPKHeader extends React.Component {
   componentDidMount() {
     const RPKHeaderContainer: any = document.querySelector('[id="RPKHeader"]');
-    console.log(document.querySelector('#RPKHeader'));
     window.addEventListener('scroll', function () {
-      if (window.pageYOffset > 0) {
+      if (window.pageYOffset > 10) {
         headerShift(RPKHeaderContainer, 0);
       } else {
         headerShift(RPKHeaderContainer, null);
@@ -23,7 +22,25 @@ class RPKHeader extends React.Component {
   render() {
     return (
       <div id='RPKHeader' className={insertClasses.join(' ')}>
-        RPKHeader
+        <div className={classes.RPKHeader__table}>
+          <table>
+            <tr>
+              <th>Статус</th>
+              <th>№ Заявки</th>
+              <th>От кого</th>
+              <th>Кому</th>
+              <th>Реализация</th>
+              <th>Банк</th>
+              <th>Расход</th>
+              <th>Поставщик</th>
+              <th>Счет</th>
+              <th>Счет фактура</th>
+              <th>Наименование</th>
+              <th>Комментарий</th>
+              <th>Расчет</th>
+            </tr>
+          </table>
+        </div>
       </div>
     );
   }
