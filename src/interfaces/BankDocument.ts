58d8e15e304ee1b7ca37088588e9b52658aaf98b
denristun <58968932+denristun@users.noreply.github.com>
2020-09-17 10:14:21 +0400

    1
    1

--- src/interfaces/BankDocument.ts
+++ src/interfaces/BankDocument.ts
@@ -1,75 +1,106 @@
-import { BankRequest } from "./BankRequest";
-import { BankRequestType } from "./BankRequestType";
+import { BankRequest } from './BankRequest'
+import { BankRequestType } from './BankRequestType'
 
-const hash = require('object-hash');
+const hash = require('object-hash')
 
-export class BankDocument{
-    date: string
-    income: number
-    outcome: number
-    destination: string
-    client: string
-    organization: string
-    requests: BankRequest[]
-    id:string
-    error?: boolean
-    requestsSum?: number
+export class BankDocument {
+  date: string
+  income: number
+  outcome: number
+  destination: string
+  client: string
+  organization: string
+  requests: BankRequest[]
+  id: string
+  error?: boolean
+  requestsSum?: number
+  type?: BankRequestType
+
+  constructor(
+    date: string,
+    income: number,
+    outcome: number,
+    destination: string,
+    client: string,
+    organization: string,
+    comment: string,
     type?: BankRequestType
-
-    constructor(date: string, income: number , outcome: number, destination: string, client: string, organization: string, comment: string, type?:BankRequestType){
-        this.date = date
-        this.income = income ? income : 0
-        this.outcome = outcome ? outcome : 0
-        this.destination = destination 
-        this.client = client
-        this.organization = organization
-        this.requests = []
-        // this.id = hash({destination:this.destination, date: this.date, value: this.income || this.outcome, organization: this.organization, client:this.client})
-        this.id = hash(this)
-        this.type = type
-        this.setRequests = comment
-      
-    }
-//Обработка ячейки комментарий
-  set setRequests(comment: string){
+  ) {
+    this.date = date
+    this.income = income ? income : 0
+    this.outcome = outcome ? outcome : 0
+    this.destination = destination
+    this.client = client
+    this.organization = organization
+    this.requests = []
+    // this.id = hash({destination:this.destination, date: this.date, value: this.income || this.outcome, organization: this.organization, client:this.client})
+    this.id = hash(this)
+    this.type = type
+    this.setRequests = comment
+  }
+  //Обработка ячейки комментарий
+  set setRequests(comment: string) {
     //Проверка заполненности ячейки
-   this.requestsSum = 0
-    if (comment){
-       
-        const requests = comment.split(';')
-        requests.forEach((request: string)=>{
-            let [requestNumber, value, client] = request.trim().split(' ')
-            if (requestNumber){
-                let valueFloat = value ? Number.parseFloat(value.replace(/,/g,'.')) : this.income || this.outcome
-                let valueInt = value && !Number.isNaN(+valueFloat) ? valueFloat : this.income || this.outcome
-                client = client ? client: this.client
-                // let type: BankRequestType = this.income ? 'income' : 'outcome'
+    this.requestsSum = 0
+    if (comment) {
+      const requests = comment.split(';')
+      requests.forEach((request: string) => {
+        let [requestNumber, value, client] = request.trim().split(' ')
+        if (requestNumber) {
+          let valueFloat = value
+            ? Number.parseFloat(value.replace(/,/g, '.'))
+            : this.income || this.outcome
+          let valueInt =
+            value && !Number.isNaN(+valueFloat)
+              ? valueFloat
+              : this.income || this.outcome
+          //   client = client ? client : this.client
+          client = comment
 
-                let requestType: BankRequestType = this.type ? this.type : (this.income ? 'income' : 'outcome')
-                // console.log('requestNumber',requestNumber)
-                const correctRequestNumber = requestNumber.match(/[а-я,А-Я]{3}-[0-9]{1,2}($|\/[0-9]{1,6})/g)[0]
-                this.requestsSum += valueInt
-                const bankRequest = new BankRequest(correctRequestNumber, valueInt, requestType, this.id, client, this.destination, this.date, this.client, "", this.organization)
-                // console.log("bankRequests", bankRequest)
-                this.requests.push(bankRequest)
-            }
-        })
-    
-        if (this.requests.length > 1){
-            this.error = Math.round((this.requestsSum + Number.EPSILON) * 100) / 100 === (this.income || this.outcome)? false : true
-            
+          let requestType: BankRequestType = this.type
+            ? this.type
+            : this.income
+            ? 'income'
+            : 'outcome'
+          // console.log('requestNumber',requestNumber)
+          const correctRequestNumber = requestNumber.match(
+            /[а-я,А-Я]{3}-[0-9]{1,2}($|\/[0-9]{1,6})/g
+          )[0]
+          this.requestsSum += valueInt
+          const bankRequest = new BankRequest(
+            correctRequestNumber,
+            valueInt,
+            requestType,
+            this.id,
+            client,
+            this.destination,
+            this.date,
+            this.client,
+            '',
+            this.organization
+          )
+          // console.log("bankRequests", bankRequest)
+          this.requests.push(bankRequest)
         }
-        else{
-            this.error = this.requests[0].value === (this.income || this.outcome)? false : true
-        }
-        }
+      })
+
+      if (this.requests.length > 1) {
+        this.error =
+          Math.round((this.requestsSum + Number.EPSILON) * 100) / 100 ===
+          (this.income || this.outcome)
+            ? false
+            : true
+      } else {
+        this.error =
+          this.requests[0].value === (this.income || this.outcome)
+            ? false
+            : true
+      }
+    }
 
     //Необходимо реализовать проверку ячейки на ошибки
-    else{
-        this.requests = []
-        }
+    else {
+      this.requests = []
     }
-
-     
-
+  }
 }
